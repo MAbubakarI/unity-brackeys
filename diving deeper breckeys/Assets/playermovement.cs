238d3c8ea0082c3c72710933d0b02dce5751f514
MAbubakarI <Abustar421@gmail.com>
2023-08-21 22:20:08 -0600

    fixed movement

--- diving deeper breckeys/Assets/playermovement.cs
+++ diving deeper breckeys/Assets/playermovement.cs
@@ -7,9 +7,14 @@ public class playermovement : MonoBehaviour
     public double playerx = 0;
     public double playery;
     public int playerz;
+    public bool faceright = true;
     //  public Vector3 playerpos;
     public Transform player;
     // public int jumptimer;
+
+    [SerializeField] private Transform groundcheck;
+    [SerializeField] private LayerMask groundlayer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +25,10 @@ public class playermovement : MonoBehaviour
     void Update()
     {
         // jumptimer -= 1;
-        playery -= 0.01;
+        if (!IsGrounded())
+        {
+            playery -= 0.01;
+        }
         if (Input.GetKey(KeyCode.D))
         {
             playerx += 0.1;
@@ -37,5 +45,28 @@ public class playermovement : MonoBehaviour
         }
         player.position = (new Vector3((float)playerx, (float)playery, playerz));
 
+        Flip();
+
+
+
     }
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundcheck.position, 0.01f, groundlayer);
+    }
+
+    void Flip() 
+    { 
+    if (Input.GetKeyDown (KeyCode.A) && faceright == true)
+        {
+            player.localScale = new Vector3(-1, 1, 1);
+
+        }
+        if (Input.GetKeyDown(KeyCode.D) && faceright == false)
+        {
+            player.localScale = new Vector3(-1, 1, 1);
+
+        }
+    }
+
 }
