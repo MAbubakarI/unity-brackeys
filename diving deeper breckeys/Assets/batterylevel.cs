1d69af59023ef2a4fff3e75c728d8362843e0e36
MAbubakarI <Abustar421@gmail.com>
2023-08-22 21:44:27 -0600

    added UI

--- diving deeper breckeys/Assets/batterylevel.cs
+++ diving deeper breckeys/Assets/batterylevel.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class batterylevel : MonoBehaviour
 
 {
 
-    public Slider slider;
+    public UnityEngine.UI.Slider  slider;
     public double battery = 100;
+    public GameObject gamepanel;
 
 
     // Start is called before the first frame update
@@ -20,8 +22,11 @@ public class batterylevel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        battery -= (double)0.005;
+        if (battery <= 0) { gamepanel.SetActive(true); }
+        else
+        { battery -= (double)0.005; }
         slider.value = (float)battery;
+        
     }
    
 }
